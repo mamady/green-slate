7496181ccf3b806ee2ae4d1e148e73d50cd162e8
Braunson <braunson@gmail.com>
2012-08-07 22:14:52 +0000

    Minor changes."

--- file.rb
+++ file.rb
@@ -1865,3 +1865,4 @@ Green slate
 Green slate
 Green slate
 Green slate
+Green slate
--- greenify.rb
+++ greenify.rb
@@ -22,9 +22,10 @@ while END_DATE - date >= 0
 
     # amend the commit date
     formatted_date = date.strftime("Thu %b %e %H:%M:%S GMT %Y");
-    cmd = %(ENV['GIT_COMMITTER_DATE']="#{formatted_date}" git commit --amend --date "#{formatted_date}" -m"Minor changes.")
+    ENV['GIT_COMMITTER_DATE']="#{formatted_date}" 
+    `git commit --amend --date "#{formatted_date}" -m"Minor changes.""`
     print '.'
-    `#{cmd}`
+
   end
 
   date += DAY_IN_SECONDS
