3a11d0dbb92e9cc2b3642fc92593ac07136541ea
Braunson <braunson@gmail.com>
2013-08-08 21:59:15 -0400

    Minor changes.

--- greenify.rb
+++ greenify.rb
@@ -22,7 +22,7 @@ while END_DATE - date >= 0
 
     # amend the commit date
     formatted_date = date.strftime("Thu %b %e %H:%M:%S GMT %Y");
-    cmd = %(:env GIT_COMMITTER_DATE="#{formatted_date}" git commit --amend --date "#{formatted_date}" -m"Minor changes.")
+    cmd = %(ENV['HOME'] GIT_COMMITTER_DATE="#{formatted_date}" git commit --amend --date "#{formatted_date}" -m"Minor changes.")
     print '.'
     `#{cmd}`
   end
